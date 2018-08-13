9c4bcbe1526f929ea014cc9141d6e11312c7a7ec
mustafadalga <25087769+mustafadalga@users.noreply.github.com>
2018-08-13 16:46:54 +0300

    Add files via upload
    Belirtilen parametrelere göre şifre oluşturan uygulama

---
+++ sifre uretici/sifre uretici/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace sifre_uretici
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnsifreuret = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facebookToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label7 = new System.Windows.Forms.Label();
+            this.nmrcsifresayısı = new System.Windows.Forms.NumericUpDown();
+            this.nmrcsifreuzunlugu = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmrcsifresayısı)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmrcsifreuzunlugu)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox1.Location = new System.Drawing.Point(238, 30);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(91, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "abcdef...";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(5, 31);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(424, 221);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Parametreler";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(8, 69);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Büyük Harfler";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(8, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Küçük Harfler";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox2.Location = new System.Drawing.Point(238, 69);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(102, 21);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "ABCDEF...";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox3.Location = new System.Drawing.Point(238, 107);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(117, 21);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "1234567890";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(8, 107);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rakamlar";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox4.Location = new System.Drawing.Point(238, 145);
+            this.checkBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(180, 21);
+            this.checkBox4.TabIndex = 6;
+            this.checkBox4.Text = "@é!\'£])$+%½&/[(}=?\\*{";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(8, 147);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Özel Karakterler";
+            // 
+            // btnsifreuret
+            // 
+            this.btnsifreuret.BackColor = System.Drawing.Color.Black;
+            this.btnsifreuret.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnsifreuret.ForeColor = System.Drawing.Color.Lime;
+            this.btnsifreuret.Location = new System.Drawing.Point(6, 387);
+            this.btnsifreuret.Name = "btnsifreuret";
+            this.btnsifreuret.Size = new System.Drawing.Size(424, 39);
+            this.btnsifreuret.TabIndex = 21;
+            this.btnsifreuret.Text = "Şifre Üret";
+            this.btnsifreuret.UseVisualStyleBackColor = false;
+            this.btnsifreuret.Click += new System.EventHandler(this.btnsifreuret_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.hakkındaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(761, 24);
+            this.menuStrip1.TabIndex = 22;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.çıkışToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.dosyaToolStripMenuItem.Text = "Dosya ";
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.çıkışToolStripMenuItem.Text = "Çıkış            Alt+F4";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // hakkındaToolStripMenuItem
+            // 
+            this.hakkındaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.facebookToolStripMenuItem});
+            this.hakkındaToolStripMenuItem.Name = "hakkındaToolStripMenuItem";
+            this.hakkındaToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.hakkındaToolStripMenuItem.Text = "Hakkında";
+            // 
+            // facebookToolStripMenuItem
+            // 
+            this.facebookToolStripMenuItem.Name = "facebookToolStripMenuItem";
+            this.facebookToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.facebookToolStripMenuItem.Text = "Hakkında";
+            this.facebookToolStripMenuItem.Click += new System.EventHandler(this.facebookToolStripMenuItem_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(22, 66);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Şifre Sayısı";
+            // 
+            // nmrcsifresayısı
+            // 
+            this.nmrcsifresayısı.Location = new System.Drawing.Point(307, 66);
+            this.nmrcsifresayısı.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.nmrcsifresayısı.Name = "nmrcsifresayısı";
+            this.nmrcsifresayısı.Size = new System.Drawing.Size(48, 23);
+            this.nmrcsifresayısı.TabIndex = 12;
+            this.nmrcsifresayısı.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nmrcsifreuzunlugu
+            // 
+            this.nmrcsifreuzunlugu.Location = new System.Drawing.Point(307, 34);
+            this.nmrcsifreuzunlugu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.nmrcsifreuzunlugu.Name = "nmrcsifreuzunlugu";
+            this.nmrcsifreuzunlugu.Size = new System.Drawing.Size(48, 23);
+            this.nmrcsifreuzunlugu.TabIndex = 8;
+            this.nmrcsifreuzunlugu.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(22, 34);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Şifre Uzunluğu";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.nmrcsifreuzunlugu);
+            this.groupBox2.Controls.Add(this.nmrcsifresayısı);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(5, 260);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(424, 110);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Şifre Uzunluğu Ve Şifre Sayısı";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(436, 37);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(310, 386);
+            this.textBox1.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(761, 448);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnsifreuret);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(767, 476);
+            this.MinimumSize = new System.Drawing.Size(767, 476);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Şifre Üretme";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmrcsifresayısı)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmrcsifreuzunlugu)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnsifreuret;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakkındaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem facebookToolStripMenuItem;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown nmrcsifresayısı;
+        private System.Windows.Forms.NumericUpDown nmrcsifreuzunlugu;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ sifre uretici/sifre uretici/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sifre_uretici
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        char[] ozelkarakterler = { '{', '<', '>', '|', '.', ':', '`', '@', ';', ',', '"', 'é', '!', '\\', '\'', '£', '$', ',', '€', '%', '½', '&', '/', '¨', '~', '{', '[', '(', ']', ')', '}', '=', '?', '*', '-', '_', '}' };
+        string ozelkarakter = "{<>|.:`@;,\"é!'£$+%½&/{[(])}=?\\*-_}";
+        Random r = new Random();
+        private void btnsifreuret_Click(object sender, EventArgs e)
+        {
+         if (checkBox1.Checked == false && checkBox2.Checked == false && checkBox3.Checked == false && checkBox4.Checked == false)
+                MessageBox.Show("Lütfen Parametrelerden en az birini işaretleyiniz");
+
+            int sifreuzunlugu = Convert.ToInt32(nmrcsifreuzunlugu.Value);
+            int sifresayısı = Convert.ToInt32(nmrcsifresayısı.Value);
+            int x;
+
+           textBox1.Clear();
+             for (int i = 0; i < sifresayısı; i++)
+            {
+                string sifre = "";
+                for (int j = 0; j < sifreuzunlugu; j++)
+                {
+                    x = r.Next(0, 4);
+                  if (x == 0)
+                    {
+                        if (checkBox1.Checked == true)
+                        {
+                            sifre += Convert.ToChar(r.Next(97, 123));
+                        }
+                        else if (checkBox2.Checked == true)
+                        {
+                            sifre += Convert.ToChar(r.Next(65, 91));
+                        }
+                        else if (checkBox3.Checked == true)
+                        {
+                            sifre += r.Next(0, 10);
+                        }
+                        else if (checkBox4.Checked == true)
+                        {
+                            sifre += Convert.ToString(ozelkarakterler[r.Next(ozelkarakterler.Length)]);
+                        }
+                    }
+                    else if (x == 1)
+                    {
+                        if (checkBox2.Checked == true)
+                        {
+                            sifre += Convert.ToChar(r.Next(65, 91));
+                        }
+                        else if (checkBox1.Checked == true)
+                        {
+                            sifre += Convert.ToChar(r.Next(97, 123));
+                        }
+                        else if (checkBox3.Checked == true)
+                        {
+                            sifre += r.Next(0, 10);
+                        }
+                        else if (checkBox4.Checked == true)
+                        {
+                            sifre += Convert.ToString(ozelkarakterler[r.Next(ozelkarakterler.Length)]);
+                        }
+                    }
+                    else if (x == 2)
+                    {
+                        if (checkBox3.Checked == true)
+                        {
+                            sifre += r.Next(0, 10);
+                        }
+                        else if (checkBox1.Checked == true)
+                        {
+                            sifre += Convert.ToChar(r.Next(97, 123));
+                        }
+                        else if (checkBox2.Checked == true)
+                        {
+                            sifre += Convert.ToChar(r.Next(65, 91));
+                        }
+                        else if (checkBox4.Checked == true)
+                        {
+                            sifre += Convert.ToString(ozelkarakterler[r.Next(ozelkarakterler.Length)]);
+                        }
+                    }
+                    else if (x == 3)
+                    {
+                        if (checkBox4.Checked == true)
+                        {
+                            sifre += Convert.ToString(ozelkarakterler[r.Next(ozelkarakterler.Length)]);
+                        }
+                        else if (checkBox3.Checked == true)
+                        {
+                            sifre += r.Next(0, 10);
+                        }
+                        else if (checkBox1.Checked == true)
+                        {
+                            sifre += Convert.ToChar(r.Next(97, 123));
+                        }
+                        else if (checkBox2.Checked == true)
+                        {
+                            sifre += Convert.ToChar(r.Next(65, 91));
+                        }
+                    }
+                }
+            textBox1.Text += sifre +"\r\n";
+            }
+        }
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void facebookToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu program Mustafa DALĞA Tarafından Kodlanmıştır");
+        }
+   }
+}
